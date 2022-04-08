f1791db28194367f93ebe82b83b5cd64eef7b708
Ajuka David <ajuka.zephiniah@gmail.com>
2022-04-08 23:03:30 +0200

    please work

--- src/main.js
+++ src/main.js
@@ -4,6 +4,4 @@ import router from './router'
 import store from './store'
 import './styles/app.css'
 
-createApp(App).use(store).use(Unsplash, {
-  accessKey: '9i6gZ0RMJr235tpYq5JXbbbGJnPRGDaWnhR_4nBuUpw'
-}).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
