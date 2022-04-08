5b036a8c6b3f88d34034510ded6055ee03e4acb2
Ajuka David <ajuka.zephiniah@gmail.com>
2022-04-08 22:55:34 +0200

    commit before build

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/app.css'
-import Unsplash from 'vue-unsplash' // Here
 
 createApp(App).use(store).use(Unsplash, {
   accessKey: '9i6gZ0RMJr235tpYq5JXbbbGJnPRGDaWnhR_4nBuUpw'
